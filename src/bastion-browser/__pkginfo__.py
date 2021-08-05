0c424cf39536abd5b64a871addc74977dd46f108
eric pellegrini <pellegrini@ill.fr>
2021-08-05 15:38:00 +0200

    added MANIFEST and __pkginfo88.py files

---
+++ src/bastion-browser/__pkginfo__.py
@@ -0,0 +1,25 @@
+# **************************************************************************
+#
+# bastion-browser
+#
+# **************************************************************************
+
+__description__ = "A software for browsing bastion proected ressources"
+
+__long_description__ = """bastion-browser is a software for browsing files on ressources behind a bastion server. It allows
+to browse, rename or remove file but also to transfer file from/to a local filesystem. As usch, it can be used as a light sftp client.
+"""
+
+__author__ = "E.C. Pellegrini"
+
+__author_email__ = "pellegrini@ill.fr"
+
+__maintainer__ = "E.C. Pellegrini"
+
+__maintainer_email__ = "pellegrini@ill.fr"
+
+__repo__ = "https://code.ill.fr/si/python-projects/bastion-browser"
+
+__license__ = "GPL 3"
+
+__version__ = "0.0.0"
