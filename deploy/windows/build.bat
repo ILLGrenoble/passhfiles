ad406b98205d4320496924c044983b029fb3d7a7
Eric Pellegrini <pellegrini@ill.fr>
2021-08-13 10:09:11 +0200

    added gitlab ci configuration file

---
+++ deploy/windows/build.bat
@@ -0,0 +1,38 @@
+@echo off
+
+set bastion_browser_dir=%cd%
+
+rem the path to the python installer executable
+set python_installer=C:\\temp\python-3.8.6.exe
+
+rem the directory that will contain the python + deps + bastion_browser
+set target_dir=C\\temp\bastion_browser_install
+
+rem uninstall python
+%python_installer% /quiet /uninstall
+
+rem remove(if existing) target directory
+rmdir /S /Q %target_dir%
+
+rem create the target directory that will contains the python installation
+mkdir %target_dir%
+
+rem install python to the select target directory
+%python_installer% /quiet TargetDir=%target_dir%
+
+rem the path to pip executable
+set pip_exe=%target_dir%\Scripts\pip.exe
+
+rem install dependencies
+%pip_exe% install -r %bastion_browser_dir%\requirements.txt
+
+rem the path to python executable
+set python_exe=%target_dir%\python.exe
+
+rem build and install bastion_browser using the python installed in the target directory
+rmdir /S /Q %bastion_browser_dir%\build
+%python_exe% setup.py build install
+
+rem copy the LICENSE and CHANGELOG files
+copy %bastion_browser_dir%\LICENSE %bastion_browser_dir%\deploy\windows
+copy %bastion_browser_dir%\CHANGELOG.md %bastion_browser_dir%\deploy\windows\CHANGELOG.txt
---
+++ deploy/windows/deploy.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set bastion_browser_dir=%cd%
+
+set version=%CI_COMMIT_REF_NAME%
+
+set target_dir=C\\temp\bastion_browser_install
+
+rem the path to nsis executable
+set makensis="C:\Program Files (x86)\NSIS\Bin\makensis.exe"
+set nsis_installer=%bastion_browser_dir%\deploy\windows\installer.nsi
+
+del /Q %target_dir%\bastion_browser-%version%-win-amd64.exe
+
+%makensis% /V4 /Onsis_log.txt /DVERSION=%version% /DARCH=win-amd64 /DTARGET_DIR=%target_dir% %nsis_installer%
