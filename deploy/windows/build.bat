524280c013f04ce5747ef107834af786c9d7f704
Eric Pellegrini <pellegrini@ill.fr>
2021-08-13 14:21:21 +0200

    added double backslash to windows path

--- deploy/windows/build.bat
+++ deploy/windows/build.bat
@@ -7,7 +7,7 @@ rem the parent to bastion_browser base directory
 set bastion_browser_dir=%2%
 
 rem the directory that will contain the python + deps + bastion_browser
-set target_dir=%bastion_browser_dir%\ci-install
+set target_dir=%bastion_browser_dir%\\ci-install
 
 rem repair before python
 %python_installer% /quiet /repair /passive
@@ -25,14 +25,14 @@ rem install python to the select target directory
 %python_installer% /quiet TargetDir=%target_dir% Include_test=0
 
 rem the path to pip executable
-set pip_exe=%target_dir%\Scripts\pip.exe
+set pip_exe=%target_dir%\\Scripts\\pip.exe
 
 rem install dependencies
-%pip_exe% install -r %bastion_browser_dir%\requirements.txt
+%pip_exe% install -r %bastion_browser_dir%\\requirements.txt
 
 rem the path to python executable
-set python_exe=%target_dir%\python.exe
+set python_exe=%target_dir%\\python.exe
 
 rem build and install bastion_browser using the python installed in the target directory
-rmdir /S /Q %bastion_browser_dir%\build
+rmdir /S /Q %bastion_browser_dir%\\build
 %python_exe% setup.py build install
--- deploy/windows/deploy.bat
+++ deploy/windows/deploy.bat
@@ -4,16 +4,16 @@ set bastion_browser_dir=%1%
 
 set version=%2%
 
-set target_dir=%bastion_browser_dir%\ci-install
+set target_dir=%bastion_browser_dir%\\ci-install
 
 rem copy the LICENSE and CHANGELOG files
-copy %bastion_browser_dir%\LICENSE %bastion_browser_dir%\deploy\windows
-copy %bastion_browser_dir%\CHANGELOG.md %bastion_browser_dir%\deploy\windows\CHANGELOG.txt
+copy %bastion_browser_dir%\\LICENSE %bastion_browser_dir%\\deploy\\windows
+copy %bastion_browser_dir%\\CHANGELOG.md %bastion_browser_dir%\\deploy\\windows\CHANGELOG.txt
 
 rem the path to nsis executable
-set makensis="C:\Program Files (x86)\NSIS\Bin\makensis.exe"
-set nsis_installer=%bastion_browser_dir%\deploy\windows\installer.nsi
+set makensis="C:\\Program Files (x86)\\NSIS\\Bin\\makensis.exe"
+set nsis_installer=%bastion_browser_dir%\\deploy\\windows\\installer.nsi
 
-del /Q %target_dir%\bastion_browser-%version%-win-amd64.exe
+del /Q %target_dir%\\bastion_browser-%version%-win-amd64.exe
 
 %makensis% /V4 /Onsis_log.txt /DVERSION=%version% /DARCH=win-amd64 /DTARGET_DIR=%target_dir% %nsis_installer%
