fb946e5a2d32f12a207528789392a5d5e4d27106
eric pellegrini <pellegrini@ill.fr>
2021-09-07 15:21:06 +0200

    added build instructions in the deploy.sh macos script

--- deploy/macos/deploy.sh
+++ deploy/macos/deploy.sh
@@ -8,13 +8,19 @@ cd ${CI_PROJECT_DIR}
 
 rm -rf ${CI_PROJECT_DIR}/venvs/passhfiles
 
+rm -rf ${CI_PROJECT_DIR}/build
+
+rm -rf ${CI_PROJECT_DIR}/dist
+
 virtualenv -p python3 ${CI_PROJECT_DIR}/venvs/passhfiles
 
 source ${CI_PROJECT_DIR}/venvs/passhfiles/bin/activate
 
 pip install .
 
-python3 setup.py py2app
+pip install py2app
+
+python3 setup.py py2app --packages cffi
 
 VERSION_NAME=`python -c "exec(open('${CI_PROJECT_DIR}/src/passhfiles/__pkginfo__.py').read()) ; print(__version__)"`
 
