61899a80e1ed0a62af0ba1133e9a5f828d1caca1
eric pellegrini <pellegrini@ill.fr>
2021-09-07 17:18:54 +0200

    fixed typo in the deploy.sh macos script

--- deploy/macos/deploy.sh
+++ deploy/macos/deploy.sh
@@ -8,7 +8,7 @@ VERSION_NAME=`python -c "exec(open('${CI_PROJECT_DIR}/src/passhfiles/__pkginfo__
 
 cd ${CI_PROJECT_DIR}/deploy/macos
 
-PASSHFILES_DMG=PASSHFILES-${VERSION_NAME}-macOS-amd64.dmg
+PASSHFILES_DMG=passhfiles-${VERSION_NAME}-macOS-amd64.dmg
 hdiutil unmount /Volumes/passhfiles -force -quiet
 sleep 5
 ${CI_PROJECT_DIR}/deploy/macos/create-dmg --background "${CI_PROJECT_DIR}/deploy/macos/resources/dmg/dmg_background.jpg" \
